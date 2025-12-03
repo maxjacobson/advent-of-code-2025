da3a59be5bf00ed0028cfad931a0ad2bc2197038
Max Jacobson <max@hardscrabble.net>
2025-12-02 22:59:38 -0500

    Day two part two but fast
    Disclosure: claude code helped me identify the performant approach, but
    I implemented it myself

--- aoc-02.fish
+++ aoc-02.fish
@@ -19,47 +19,81 @@ function id_invalid_part_one
     end
 end
 
-function id_invalid_part_two
+function invalid_part_one_ids
     set --local id $argv[1]
-    set --local length (string length $id)
-
-    for sub_string_length in (seq 1 (math "$length - 1"))
-        if test (math "$length % $sub_string_length") -ne 0
-            continue
-        end
-
-        set --local sub_string (string sub --length $sub_string_length $id)
-
-        set --local repetitions (math "$length / $sub_string_length")
-
-        if test (string repeat --count "$repetitions" "$sub_string") -eq "$id"
-            return 0
-        end
-    end
-
-    false
-end
-
-string join '' -- "There are " (count $ranges) " ranges"
-
-set --local progress 1
-for range in $ranges
-    string join '' -- "[" (date) "]" " checking range $progress"
-    set --local ids (string split - "$range")
-    set --local stop (string trim $ids[2])
-
-    set --local id (string trim $ids[1])
+    set --local stop $argv[2]
 
     while test $id -le $stop
-        # if id_invalid_part_one "$id"
-        #     set part_one_result (math "$part_one_result + $id")
-        # end
-
-        if id_invalid_part_two "$id"
-            set part_two_result (math "$part_two_result + $id")
+        if id_invalid_part_one "$id"
+            echo "$id"
         end
 
         set id (math "$id + 1")
+    end
+end
+
+function factors_for
+    set --local num $argv[1]
+
+    for n in (seq 1 $num)
+        set --local x (math "$num % $n")
+        if test $x -eq 0
+            if test $n -ne $num
+                echo $n
+            end
+        end
+    end
+end
+
+function candidates_for
+    set --local candidate_length $argv[1]
+    set --local substring_length $argv[2]
+    set --local repetitions (math "$candidate_length / $substring_length")
+    set --local ceil (string repeat "9" -n "$substring_length")
+
+    for n in (seq 1 $ceil)
+        string repeat $n -n $repetitions
+    end
+end
+
+function invalid_part_two_ids
+    set --local start $argv[1]
+    set --local stop $argv[2]
+
+    set --local start_digits (string length $start)
+    set --local stop_digits (string length $stop)
+
+    set --local results
+
+    for digit in (seq $start_digits $stop_digits)
+        for factor in (factors_for $digit)
+            for candidate in (candidates_for $digit $factor)
+                if test $candidate -ge $start
+                    if test $candidate -le $stop
+
+                        if ! contains $candidate $results
+                            set --append results $candidate
+                            echo $candidate
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+
+set --local progress 1
+for range in $ranges
+    set --local ids (string split - "$range")
+    set --local start (string trim $ids[1])
+    set --local stop (string trim $ids[2])
+
+    for invalid_part_one_id in (invalid_part_one_ids "$start" "$stop")
+        set part_one_result (math "$part_one_result + $invalid_part_one_id")
+    end
+
+    for invalid_part_two_id in (invalid_part_two_ids "$start" "$stop")
+        set part_two_result (math "$part_two_result + $invalid_part_two_id")
     end
 
     set progress (math "$progress + 1")
