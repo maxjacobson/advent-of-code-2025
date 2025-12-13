c1b8dcb71dbdf8c67200f008c1ac0eaf8a5080e5
Max Jacobson <max@hardscrabble.net>
2025-12-13 13:27:58 -0500

    day 3 part 2

--- aoc-03.fish
+++ aoc-03.fish
@@ -1,6 +1,3 @@
-set --local input (cat $argv[1])
-set --local part_one_result 0
-
 function highest_digit
     set --local digits (string split "" $argv[1])
 
@@ -24,17 +21,46 @@ end
 
 function maximum_joltage_for
     set --local bank $argv[1]
-    set --local bank_without_last_digit (string sub --end -1 $bank)
-    set --local highest_first_digit_info (highest_digit $bank_without_last_digit)
-    set --local highest_second_digit_info (highest_digit (string sub --start (math "$highest_first_digit_info[2] + 1") $bank))
 
-    echo "$highest_first_digit_info[1]$highest_second_digit_info[1]"
+    set --local remaining_digits $argv[2]
+    set --local untouchable_amount (math "$remaining_digits - 1")
+    set --local candidates
+
+    if test $remaining_digits -eq 1
+        set candidates "$bank"
+    else
+        set candidates (string sub --end "-$untouchable_amount" "$bank")
+    end
+
+    set --local highest_digit_among_candidates_info (highest_digit "$candidates")
+
+    set --local remaining_bank_starting_point (math "$highest_digit_among_candidates_info[2] + 1")
+    set --local remaining_bank (string sub "$bank" --start "$remaining_bank_starting_point")
+
+    if test $remaining_digits -eq 1
+        echo $highest_digit_among_candidates_info[1]
+    else
+        set --local digits_to_follow (maximum_joltage_for "$remaining_bank" (math "$remaining_digits - 1"))
+
+        echo "$highest_digit_among_candidates_info[1]$digits_to_follow"
+    end
 end
 
-for bank in $input
-    set --local maximum_joltage (maximum_joltage_for $bank)
+function main
+    set --local input (cat $argv[1])
+    set --local part_one_result 0
+    set --local part_two_result 0
 
-    set part_one_result (math "$part_one_result + $maximum_joltage")
+    for bank in $input
+        set --local part_one_maximum_joltage (maximum_joltage_for $bank 2)
+        set --local part_two_maximum_joltage (maximum_joltage_for $bank 12)
+
+        set part_one_result (math "$part_one_result + $part_one_maximum_joltage")
+        set part_two_result (math --scale=0 "$part_two_result + $part_two_maximum_joltage")
+    end
+
+    echo "Part one: $part_one_result" # 17376
+    echo "Part two: $part_two_result" # 172119830406258
 end
 
-echo "Part one: $part_one_result"
+main $argv[1]
