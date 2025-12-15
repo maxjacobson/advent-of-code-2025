533f5bf9b398f762e46b71da358844d15a751219
Max Jacobson <max@hardscrabble.net>
2025-12-14 23:45:33 -0500

    Day four part two
    This fully took 35 minutes to run 😬

--- aoc-04.fish
+++ aoc-04.fish
@@ -62,8 +62,24 @@ function neighboring_paper_rolls
     echo $result
 end
 
+function remove_paper_roll
+    set --local x $argv[1]
+    set --local y $argv[2]
+    set --local map $argv[3]
+
+    set --local lines (string split " " "$map")
+    set --local line $lines[$y]
+    set --local chars (string split "" "$line")
+    set chars[$x] "."
+    set lines[$y] (string join "" $chars)
+    string join " " $lines
+end
+
 function main
+    set --local map (cat $argv[1])
+
     set --local part_one_result 0
+    set --local part_two_result 0
 
     set --local map (cat $argv[1])
 
@@ -77,6 +93,27 @@ function main
     end
 
     echo "Part one: $part_one_result"
+
+    while true
+        set --local did_remove_any nope
+
+        for paper_roll in (analyze_paper_rolls "$map")
+            set --local coordinates (string split "," "$paper_roll")
+            set --local count (neighboring_paper_rolls "$coordinates[1]" "$coordinates[2]" "$map")
+
+            if test "$count" -lt 4
+                set map (remove_paper_roll "$coordinates[1]" "$coordinates[2]" "$map")
+                set did_remove_any yep
+                set part_two_result (math "$part_two_result + 1")
+            end
+        end
+
+        if test "$did_remove_any" = nope
+            break
+        end
+    end
+
+    echo "Part two: $part_two_result"
 end
 
 main "$argv[1]"
